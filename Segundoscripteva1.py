24ba02ede1196650db915b53d3c04f602554a7d1
ignacio.cabeza <nacciojavier@gmail.com>
2024-04-15 02:45:22 +0000

    primerscript.txt

---
+++ Segundoscripteva1.py
@@ -0,0 +1,8 @@
+nombre = input("Ingrese su nombre")
+apellido = input("Ingrese su apellido")
+seccion = input("Ingrese su seccion")
+sede = input("Ingrese su sede")
+print(nombre)
+print(apellido)
+print(seccion)
+print(sede)
